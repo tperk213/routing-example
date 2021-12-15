f367c88fcea66ea7e9d8aba4a188482c2b26a431
tom perkins <t.perk@live.com.au>
2021-12-16 10:02:09 +1100

    Updates

---
+++ precache-manifest.f3b28b22be40d304f605c81fdb95a18c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dee17f5dcff77454547a",
+    "url": "/routing-example/static/css/main.07642c2b.chunk.css"
+  },
+  {
+    "revision": "dee17f5dcff77454547a",
+    "url": "/routing-example/static/js/main.dee17f5d.chunk.js"
+  },
+  {
+    "revision": "4ce564cb67a3d3ded274",
+    "url": "/routing-example/static/js/1.4ce564cb.chunk.js"
+  },
+  {
+    "revision": "31ab9cc6ee137a860ce1",
+    "url": "/routing-example/static/js/2.31ab9cc6.chunk.js"
+  },
+  {
+    "revision": "8176f059d09b55f39499",
+    "url": "/routing-example/static/js/runtime~main.8176f059.js"
+  },
+  {
+    "revision": "cfe30c269403e1976bbfed4d4f33f084",
+    "url": "/routing-example/index.html"
+  }
+];
